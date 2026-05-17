faf68a41d73e5421112b75bc7bbddd990054db86
HishamHamdoon <hamdon_22@hotmail.com>
2026-05-17 22:48:08 +0300

    add new command to print specific column of process

--- get-current-process.sh
+++ get-current-process.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#####################
+#Author Hisham 
+#date May 17 2026 22:44:00
+#set -x
 echo 'the current process are:'
+#this command is printing running process
 ps -ef
+#this command printing specific column based on awk option 
+ps -ef | awk -F" " '{print $2}'
